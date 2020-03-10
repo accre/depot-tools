a0e734ac07f9877e26bbd1572826e7e6dc7b3aa0
root <root@huginn.reddnet.org>
2020-03-10 13:33:06 -0500

    * Fix bug in count_drives.py where it would fail to count more than one sequestered drive

--- count_drives.py
+++ count_drives.py
@@ -31,9 +31,13 @@ for line in SysExec("get_version -a").splitlines():
 	if re.search("^RID: ", line):
 		IBP_Rids.append(line.split(" ")[1])
 
-	if re.search("Pending RID count", line):
+	if re.search("Pending RID list", line):
+
+#		print("DEBUG:  line = " + str(line))
 
 		Rids = line.split(":")[1].strip().split(" ")
+
+#		print("DEBUG:  Rids = " + str(Rids))
 
 		if Rids[0] == "0":
 			continue
@@ -41,9 +45,11 @@ for line in SysExec("get_version -a").splitlines():
 		for Rid in Rids:
 			IBP_Rids.append(int(Rid))
 			Count_Pending += 1
+
+#print("DEBUG:  IBP_Rids = " + str(IBP_Rids))
+
 IBP_Rids.sort()
 Num_IBP = len(IBP_Rids)
-
 
 # Get a list of RIDs visible to the OS via /sys/block
 OS_Rids = []
@@ -57,26 +63,37 @@ for disk in LS:
 OS_Rids.sort()
 Num_BLK = len(OS_Rids)
 
+#print("DEBUG:  OS_Rids = " + str(OS_Rids))
+
 # See if there descrepancies between OS and IBP Server
 Diff_Rids = list(set(OS_Rids) - set(IBP_Rids))
 
+#print("DEBUG:  Diff_Rids = " + str(Diff_Rids))
+
 # If so, see if they are sequestered
-Count_Seq = 0
+Num_Seq = 0
 if Diff_Rids:
-	for Rid in Diff_Rids:
+	for Rid in list(Diff_Rids):
+#		print("DEBUG:  Checking Rid " + str(Rid))
 		Status = RID_Check_Sequester(Rid)
+#		print("DEBUG:  Rid " + str(Rid) + " status = " + str(Status))
 		if Status.startswith("SEQUESTERED"):
-			Count_Seq += 1
+			Num_Seq += 1
 			Diff_Rids.remove(Rid)
+#			print("DEBUG:  Removing " + str(Rid) + " from Diff_Rids, leaving " + str(Diff_Rids))
 
-if Num_NS == Num_IBP and Num_NS == Num_BLK:
+#print("DEBUG:  Diff_Rids = " + str(Diff_Rids))
 
-	if Count_Pending == 0:
+#print("DEBUG:  Num_NS = " + str(Num_NS) + " and Num_IBP + Num_Seq = " + str(Num_IBP + Num_Seq) + " and Num_BLK = " + str(Num_BLK))
+
+if Num_NS == (Num_IBP + Num_Seq) and Num_NS == Num_BLK:
+
+	if Num_Seq == 0:
 
 		print("All Drives = " + str(Num_NS))
 	else:
-		print("All Drives = " + str(Num_NS) + " (including " + str(Count_Seq) + " sequestered drives)")
+		print("All Drives = " + str(Num_NS) + " (including " + str(Num_Seq) + " sequestered drives)")
 else:
-	print("GV = " + str(Num_IBP) + " and NS = " + str(Num_NS) + " and SN = " + str(Num_BLK) + " and SEQUESTERED = " + str(Count_Seq) + " and PENDING = " + str(Count_Pending))
+	print("GV = " + str(Num_IBP) + " and NS = " + str(Num_NS) + " and SN = " + str(Num_BLK) + " and SEQUESTERED = " + str(Num_Seq) + " and PENDING = " + str(Count_Pending))
 	if Diff_Rids:
 		print("DIFF_RID = " + str(Diff_Rids))
