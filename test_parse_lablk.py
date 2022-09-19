cc211032ed81a344ba00fd5f9c3eeab668ebb2ae
root <root@afs-accre04.vampire>
2022-09-19 12:59:24 -0500

    * Sync in case I have to move off this server

---
+++ test_parse_lablk.py
@@ -0,0 +1,477 @@
+#!/usr/bin/env python3
+
+### Map drive -> enclosure/slot using sg_ses (hopefully a generic method that works on most/all depots without modification)
+
+import re
+import os
+import sys
+import math
+import time
+
+#from prettytable import PrettyTable
+
+# Note:  Any time you can avoid using Popen is a huge win time-wise
+from subprocess import Popen, PIPE, STDOUT
+
+# Cache info
+CacheDataArray = {}
+CacheTimeArray = {}
+
+# Enable/disable debugging messages
+Print_Debug = True
+
+
+def Debug(text):
+
+        """
+        Print debugging info on a single line.
+        """
+
+        if Print_Debug:
+                print("DEBUG: " + text)
+
+
+def SysExec(cmd):
+
+        """
+        Run the given command and return the output
+        """
+
+        # Cache the output of the command for 20 seconds
+        Cache_Expires = 20
+
+        # Computed once, used twice
+        Cache_Keys = list(CacheDataArray.keys())
+        if cmd in Cache_Keys:
+                Cache_Age  = time.time() - CacheTimeArray[cmd]
+        else:
+                Cache_Age  = 0
+
+        Return_Val = "ERROR"
+
+        # If we have valid data cached, return it
+        if cmd in Cache_Keys and Cache_Age < Cache_Expires:
+                Return_Val = CacheDataArray[cmd]
+
+        # If the cmd is "cat", use fopen/fread/fclose to open it and
+        # cache it as we go
+        elif not cmd in Cache_Keys and cmd.split()[0] == "cat":
+                f = open(cmd.split()[1], "r")
+                CacheDataArray[cmd] = f.read()
+                CacheTimeArray[cmd] = time.time()
+                f.close()
+                Return_Val = CacheDataArray[cmd]
+
+        # If we don't have cached data, or it's too old, regenerate it
+        elif not cmd in Cache_Keys or Cache_Age > Cache_Expires:
+                CacheDataArray[cmd] = Popen(cmd.split(), stdout=PIPE, stderr=STDOUT).communicate()[0]
+                CacheTimeArray[cmd] = time.time()
+                Return_Val = CacheDataArray[cmd]
+
+        if str(type(Return_Val)) == "<class 'bytes'>":
+                Return_Val = Return_Val.decode("utf-8")
+
+        return(Return_Val)
+
+
+def which(program):
+
+        """
+
+        Functions similar to the 'which' program in Unix.  Given
+        an executable filename, it will return the whole path to
+        that executable.
+
+        which("ls") should return "/bin/ls"
+
+        Will print an error message and terminate the program if
+        it can't locate the executable in the path.
+
+        """
+
+        def is_exe(fpath):
+                return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
+        def ext_candidates(fpath):
+                yield fpath
+                for ext in os.environ.get("PATHEXT", "").split(os.pathsep):
+                        yield fpath + ext
+
+        fpath, fname = os.path.split(program)
+
+        if fpath:
+                if is_exe(program):
+                        return program
+        else:
+                for path in os.environ["PATH"].split(os.pathsep):
+                        exe_file = os.path.join(path, program)
+                        for candidate in ext_candidates(exe_file):
+                                if is_exe(candidate):
+                                        return candidate
+
+def parse_lsblk():
+
+	"""
+	Return info on all block devices and partitions.
+	Note:  RHEL8 has an ancient version of "lsblk" compared to Ubuntu, and is missing some useful fields. :-/
+	"""
+
+	map = {}
+
+	for line in SysExec(" lsblk -O -P").splitlines():
+
+		# Parse all the keyval pairs
+		keyval  = dict(re.findall(r'(\w+)="([^"]+)"', line))
+
+		# Fetch the sd_dev device and delete it from the keyval pair
+		name = keyval["NAME"]
+		del keyval['NAME']
+
+		map["/dev/" + name] = keyval
+
+	# Convert keys to lowercase
+	lc_map = dict((k.lower(), v) for k, v in map.items())
+
+	return(lc_map)
+
+
+def parse_lsblk_disks():
+
+	"""
+	parse_lsblk() returns info on disks, partitions, and possibly other things.   Use this to filter it down to just disks
+
+	"""
+
+	map  = parse_lsblk()
+	map2 = parse_lsblk()
+
+	for key in map.keys():
+		if map[key]["type"] != "disk":
+			del map2[key]
+
+	return(map2)
+
+
+def parse_lsscsi():
+
+	"""
+	Parse the output of "lssci -gUN" into a dict
+	"""
+
+	map = {}
+
+	for line in SysExec("lsscsi -gUN").splitlines():
+
+		line = ' '.join(line.split())
+
+		hctl    = line.split()[0]
+		type    = line.split()[1]
+		sas_wwn = line.split()[2]
+		sd_dev  = line.split()[3]
+		sg_dev  = line.split()[4]
+
+		map[sd_dev] = {}
+		map[sd_dev]["sg_dev"]  = sg_dev
+		map[sd_dev]["hctl"]    = hctl
+		map[sd_dev]["sas_wwn"] = sas_wwn
+		map[sd_dev]["type"]    = type
+
+	return(map)
+
+
+def map_sata_wwn_to_hba_wwn():
+
+	"""
+	With SATA drives the HBA or backplane sometimes lies about the WWN of the actual drive.
+	Get a list of all drives (so we can map sd_devices to their sg_devices), and then
+	query sg_vpd --page=di <sg_dev> to find the mapping between fake WWN and real WWN.
+	"""
+
+	map_sata_wwn_to_hba_wwn = {}
+
+	for sd_dev in map_sd_to_sg:
+
+		Debug("map_sata_wwn_to_hba_wwn()::  Scanning sd_dev " + str(sd_dev))
+
+		sg_dev = map_sd_to_sg[sd_dev]
+
+		wwn_list = []
+
+		for line in SysExec("sg_vpd --page=di " + str(sg_dev)).splitlines():
+
+			if not re.search("0x5", line):
+ 				continue
+
+			wwn_list.append(line.strip())
+
+		if len(wwn_list) != 2:
+			continue
+
+		Debug("map_sata_wwn_to_hba_wwn()::  wwn_list for " + str(sd_dev) + " = " + str(wwn_list))
+
+		map_sata_wwn_to_hba_wwn[wwn_list[1]] = wwn_list[0]
+
+	return(map_sata_wwn_to_hba_wwn)
+
+
+def GetLocateLEDState(This_Backplane, This_Slot):
+
+        """
+        This function returns the state of the Locate LED on the given backplane/slot
+        """
+
+        This_LedState = SysExec("sg_ses -I " + str(This_Slot) + " --get=ident " + This_Backplane).strip()
+
+        LedState_Descr = "Unknown"
+        if This_LedState == "1":
+                LedState_Descr = "On"
+        if This_LedState == "0":
+                LedState_Descr = "Off"
+
+        return(LedState_Descr)
+
+
+def is_multipath_enabled():
+
+	"""
+	Probe server and see if multipath is enabled.  True if yes, False if no.
+	"""
+
+	# Verify if the multipath binary exists in the PATH.  If not, then
+	# it's not running multipath
+	if not which("multipath"):
+		return(False)
+
+	multipath_ll = SysExec("multipath -ll")
+	if re.search("DM multipath kernel driver not loaded", multipath_ll):
+		return(False)
+
+	# Now query multipath and see if it's actually running in multipath
+	# mode or not
+	num_lines = sum(1 for line in multipath_ll.splitlines())
+
+	if num_lines == 0:
+		return(False)
+	else:
+		return(True)
+
+
+def map_dm_to_mpath():
+
+        map = {}
+
+        for line in SysExec("ls -alh /dev/mapper").splitlines():
+
+                if not re.search("mpath", line):
+                        continue
+
+                line = " ".join(line.split())
+
+                dm_dev = line.split()[-1].split("/")[1]
+
+                mpath_dev = line.split()[-3]
+                mpath_dev = re.sub("-part1", "", mpath_dev)
+                mpath_dev = re.sub("-part2", "", mpath_dev)
+
+                map[dm_dev] = mpath_dev
+#                Debug("map_dm_to_mpath()::  dm_dev = " + str(dm_dev) + " and mpath_dev = " + str(mpath_dev))
+
+        return(map)
+
+
+def map_dm_to_sd_dev():
+
+        map = {}
+
+        for line in SysExec("ls -1 /sys/block").splitlines():
+                if not re.search("^dm", line):
+                        continue
+
+                dm_dev = line
+                sd_dev = SysExec("ls -1 /sys/block/" + str(dm_dev) + "/slaves")
+                sd_dev = sd_dev.replace("\n", ",").strip().rstrip(",")
+
+                if re.search("dm-", sd_dev):
+                        continue
+
+                map[dm_dev] = sd_dev
+ #               Debug("map_dm_to_sd_dev():: dm_dev = " + str(dm_dev) + " and sd_dev = " + str(sd_dev))
+
+        return(map)
+
+
+def map_mpath_to_sd_dev():
+
+        map = {}
+
+        map_1 = map_dm_to_sd_dev()
+        map_2 = map_dm_to_mpath()
+
+        dict_1 = dict(map_1)
+        dict_2 = dict(map_2)
+
+        for dm in sorted(dict_1):
+
+                sd_dev = map_1[dm]
+                mpath  = map_2[dm]
+
+                map[mpath] = sd_dev
+
+#               Debug("map_mpath_to_sd_dev()::  dm = " + str(dm) + " and mpath = " + str(mpath) + " and sd_dev = " + str(sd_dev))
+
+        return(map)
+
+
+def map_Dev_to_RID():
+
+        """
+        Return a map of /dev/ entry -> RID
+        """
+
+        Debug("def map_Dev_to_RID() entry")
+
+        Map = {}
+
+        output_ridlist = SysExec("ls -alh /dev/disk/by-label")
+
+        map_dm_mpath = map_dm_to_mpath()
+
+        for line in output_ridlist.splitlines():
+
+                if not re.search("rid-data-", line):
+                        continue
+
+                This_Dev = line.split(" ")[-1]
+                This_Dev = This_Dev.split("/")[2]
+
+                if re.search("^sd", This_Dev):
+                        This_Dev = re.sub("[0-9]*$", "", This_Dev)
+                This_Dev = "/dev/" + This_Dev
+
+                This_Rid = line.split(" ")[-3]
+                This_Rid = This_Rid.split("-")[2]
+
+                Debug("map_Dev_to_Rid()::  Dev " + This_Dev + " = Rid " + This_Rid)
+
+                if multipath_active:
+                        This_Dev = This_Dev.split("/")[2]
+                        This_Dev = map_dm_mpath[This_Dev]
+                        This_Dev = "/dev/mapper/" + This_Dev
+
+                Map[This_Dev] = This_Rid
+
+        Debug("def map_Dev_to_RID() exit")
+
+        return(Map)
+
+
+def map_enclosures():
+
+	"""
+	Loop over the enclosures and pull together some info on them.
+	"""
+
+	map_lsscsi = parse_lsscsi()
+
+	print("DEBUGDEBUG:  map_lsscsi = " + str(map_lsscsi))
+
+	map = {}
+
+	for sd_dev, dict in map_lsscsi.items():
+
+		if not re.search("enclosu", dict["type"]):
+			continue
+
+		hctl   = dict["hctl"]
+		wwn    = dict["sas_wwn"]
+		sg_dev = dict["sg_dev"]
+
+		# WWN maps to the backplane, while HCTL and SG_Dev map depend on number of HBA's.  For a depot with N hba's, they will have N unique values.
+		if not sg_dev in map:
+			map[sg_dev] = {}
+
+		map[sg_dev]["wwn"]  = wwn
+		map[sg_dev]["hctl"] = hctl
+
+		# Figure out if it's the "front" or "back" backplane
+		output = SysExec("sg_ses -p aes " + str(sg_dev)).splitlines()
+		num_slots = 0
+		for i in output:
+
+			if re.search("Element type: SAS expander", i):
+				break
+
+			if re.search("Element index:", i):
+				num_slots = num_slots + 1
+
+
+		map_lsscsi[sd_dev]["num_slots"] = num_slots
+
+		if num_slots == 12:
+			map_lsscsi[sd_dev]["alias"] = "Back"
+
+		if num_slots == 24:
+			map_lsscsi[sd_dev]["alias"] = "Front"
+
+		for line in SysExec("sg_ses -p cf " + sg_dev).splitlines():
+
+			if re.search("enclosure vendor:", line):
+				enc_vendor = re.sub("vendor:", ":", line)
+				enc_vendor = enc_vendor.split(":")[1].strip()
+				map_lsscsi[sd_dev]["enc_vendor"] = enc_vendor
+
+			if re.search("product:", line):
+				enc_product = re.sub(" rev:", ":", line)
+				enc_product = enc_product.split(":")[2].strip()
+				map_lsscsi[sd_dev]["enc_product"] = enc_product
+
+	return(map_lsscsi)
+
+
+def Return_SD_Dev(wwn_1, map):
+
+	"""
+	Match the given wwn and return the sd_dev that belongs to it
+	"""
+
+	if wwn_1 == "EMPTY":
+		return("EMPTY")
+
+	for sd_dev in map.keys():
+
+		wwn_2 = map[sd_dev]["sas_wwn"]
+
+		# Now, the actual Serial may be Serial, or Serial +/- 3.   I wish I understood the logic here better.
+		wwn_m1 = hex(int(wwn_2, 16) - 1)
+		wwn_p1 = hex(int(wwn_2, 16) + 1)
+		wwn_m2 = hex(int(wwn_2, 16) - 2)
+		wwn_p2 = hex(int(wwn_2, 16) + 2)
+
+		Serial_list = [ wwn_m2, wwn_m1, wwn_2, wwn_p1, wwn_p2 ]
+
+		if wwn_1 in Serial_list:
+			return(sd_dev)
+
+	return("UNKNOWN")
+
+
+### Main()
+
+# Use lsblk to report info on all detected block devices.
+# map_lsblk will be the primary data structure of this script, and others will be used to augment it before printing
+map_lsblk = parse_lsblk_disks()
+Debug("main:: map_lsblk = " + str(map_lsblk))
+
+# Parse "lsscsi" output and add useful fields to map_lsblk
+map_sd_to_sg = parse_lsscsi()
+Debug("main:: map_sd_to_sg = " + str(map_sd_to_sg))
+for sd_dev, dict in map_sd_to_sg.items():
+	map_lsblk[sd_dev]["sg_dev"]     = dict["sg_dev"]
+	map_lsblk[sd_dev]["hctl"]       = dict["hctl"]
+	map_lsblk[sd_dev]["lsscsi_wwn"] = dict["sas_wwn"]
+
+# Add the locate LED status to map_lsblk
+#for sd_dev in map_lsblk.keys():
+#	backplane =
+#	slot      =
+#	map_lsblk[sd_dev]["locate_led"] = GetLocateLEDState(backplane, slot):
